722f9e49f4ddc51c25c3939fc6ab36845aaa7a39
Mayara Ramos <maya.r.rebelo@gmail.com>
2018-08-02 13:11:02 -0300

    map with location access

--- js/app.js
+++ js/app.js
@@ -3,10 +3,36 @@ $(document).ready(function() {
   $('.logo-splash').delay('5000').fadeOut('slow')
 });
 
-var map;
+// Google Maps
+var map, infoWindow;
 function initMap() {
   map = new google.maps.Map(document.getElementById('map'), {
     center: {lat: -23.5576413, lng: -46.6623001},
     zoom: 15
   });
+  infoWindow = new google.maps.InfoWindow;
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function(position) {
+      var pos = {
+        lat: position.coords.latitude,
+        lng: position.coords.longitude
+      };
+
+      infoWindow.setPosition(pos);
+      infoWindow.setContent('Location found.');
+      infoWindow.open(map);
+      map.setCenter(pos);
+    }, function() {
+      handleLocationError(true, infoWindow, map.getCenter());
+    });
+  } else {
+    handleLocationError(false, infoWindow, map.getCenter());
+  }
+}
+function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+  infoWindow.setPosition(pos);
+  infoWindow.setContent(browserHasGeolocation ?
+                        'Error: The Geolocation service failed.' :
+                        'Error: Your browser doesn\'t support geolocation.');
+  infoWindow.open(map);
 }
