566d5557c34cd58434f49be8a5aef099a2294108
Mayara Ramos <maya.r.rebelo@gmail.com>
2018-08-02 11:52:04 -0300

    added main map and header

--- js/app.js
+++ js/app.js
@@ -1,4 +1,12 @@
 
 $(document).ready(function() {
-  $('.pls').delay('5000').fadeOut('slow')
+  $('.logo-splash').delay('5000').fadeOut('slow')
 });
+
+var map;
+function initMap() {
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: -23.5576413, lng: -46.6623001},
+    zoom: 15
+  });
+}
